a8c72280f03c8b2926bfa2023695d81dae5ddbcc
vnatyreromchiss <rdg2039@gmail.com>
2022-12-06 22:04:00 -0600

    chill

---
+++ src/models/figures/Figure.ts
@@ -0,0 +1,45 @@
+import { Colors } from "../Colors";
+import logo from '../../assets/black-king.png'
+import { Cell } from './../Cell';
+
+export enum FigureNames {
+    FIGURE = "Figure",
+    KING = "King", //король
+    KNIGHT = "Knight", //конь
+    PAWN = "Pawn", //пешка
+    QUEEN = "Queen", //ферзь
+    ROOK = "Rook", //ладья
+    BISHOP = "Bishop", //слон
+}
+
+export class Figure {
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: number;
+
+
+    constructor(color:Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureNames.FIGURE;
+        this.id = Math.random();
+    }
+
+    canMove(target: Cell) : boolean {
+        if(target.figure?.color === this.color) 
+            return false
+        if(target.figure?.name === FigureNames.KING)
+            return false
+        return true;    
+     
+    }
+    moveFigure(target: Cell) {
+
+    }
+
+
+}
